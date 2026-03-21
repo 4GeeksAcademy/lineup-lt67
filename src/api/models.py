281ec233e0c2eba266bb2712f56a99fd519a851f
Humberto Sánchez <30481822+imHumberto@users.noreply.github.com>
2026-03-21 20:00:28 +0000

    agregue modelo administradr models.py

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,19 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Administrador(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[int] = mapped_column(String(129), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
