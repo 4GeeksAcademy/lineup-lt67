6f59dd16fa6f574eec130ed1ddc2244ac254bcec
ABarboza7 <164107449+ABarboza7@users.noreply.github.com>
2026-03-21 23:42:58 +0000

    correcion tabla tipo

--- src/api/models.py
+++ src/api/models.py
@@ -36,7 +36,7 @@ class Administrador(db.Model):
     
 class Tipo(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-   models nombre: Mapped[str] = mapped_column(String(100), nullable=False)
+    nombre: Mapped[str] = mapped_column(String(100), nullable=False)
     descripcion: Mapped[str] = mapped_column(String(250), nullable=True)
 
     def serialize(self):
