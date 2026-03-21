f1c2a46b67912238e26fe051a591944b8c5b5960
Juan San Cristobal <130845154+Juan-MSCC@users.noreply.github.com>
2026-03-21 15:35:19 +0000

    Halfway through adding the endpoints for client CRUD

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,12 @@
+from datetime import datetime, timezone
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    nombre_completo: Mapped[str] = mapped_column(String(120), nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
@@ -17,4 +17,24 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Client(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    full_name: Mapped[str] = mapped_column(String(120), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.now(timezone.utc)
+    )
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "full_name": self.full_name,
+            "email": self.email,
+            "created_at": self.created_at.isoformat()
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,10 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Client
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from sqlalchemy import select
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +21,44 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/clients', methods=['GET'])
+def get_clients():
+
+    all_clients = list(db.session.execute(select(Client)).scalars().all())
+    all_clients = list(map(lambda client: client.serialize(), all_clients))
+    print(all_clients)
+
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    }
+
+    return jsonify(all_clients), 200
+
+@api.route('/clients', methods=['POST'])
+def post_client():
+
+    all_clients = list(db.session.execute(select(Client)).scalars().all())
+    all_clients = list(map(lambda client: client.serialize(), all_clients))
+    
+
+    body = request.get_json()
+    if body is None:
+        return jsonify({"error": "Request body can't be empty"}), 400
+    if not body.get("email"):
+        return jsonify({"error": "An Email is required"}), 400
+    if not body.get("password"):
+        return jsonify({"error": "A password is required"}), 400
+
+    client = db.session.execute(select(Client).where(Client.email == body['email'])).scalar_one_or_none()
+    if client:
+        return jsonify({"error": "There is already an account with this Email"}), 400
+
+    new_client = Client(
+        email=body["email"],
+        password=body["password"]
+    )
+    db.session.add(new_client)
+    db.session.commit()
+    return jsonify(new_client.serialize()), 201
