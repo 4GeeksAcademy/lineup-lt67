51070d230ec749a96ad7ef058681ee9022407471
Humberto Sánchez <30481822+imHumberto@users.noreply.github.com>
2026-03-23 22:23:06 +0000

    navbar y administradores.jsx listo

---
+++ src/front/pages/Administradores.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+
+export const Administradores = () => {
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const { store, dispatch } = useGlobalReducer();
+
+    function getAdministradores(){
+        fetch(backendUrl + "/api/administrador")
+        .then((response) => {
+            if (!response.ok){
+                throw new Error(response.status)
+            }
+            return response.json()
+        })
+        .then((data) => {
+            dispatch({ type: "set_administradores", payload: data })
+            console.log(store.clients)
+        })
+    }
+
+    useEffect(() =>{
+        getAdministradores()
+    },[])
+
+    return(
+        <div className="container-fluid mx-4">
+            <div className="container d-flex justify-content-between align-items-center mb-3">
+                <h2>Administradores</h2>
+            </div>
+            {store.administradores.length === 0 ? 
+            (<h4>No hay administradores</h4>) :
+            (store.administradores.map((admin) => {
+                return(
+                    <div className="container d-flex justify-content-between align-items-center my-2 border" key={admin.id}>
+                        <p>{admin.name}</p>
+                        <p>{admin.email}</p>
+                    </div>
+                )
+            }))}
+        </div>
+    )
+}
