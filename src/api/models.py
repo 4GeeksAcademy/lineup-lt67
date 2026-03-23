5deb2b2f1d819cc92c0c961df55f9e9897256162
Humberto Sánchez <30481822+imHumberto@users.noreply.github.com>
2026-03-23 21:53:54 +0000

    Delete and Post update

--- src/api/models.py
+++ src/api/models.py
@@ -21,7 +21,7 @@ class User(db.Model):
     
 class Administrador(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[int] = mapped_column(String(129), nullable=False)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
