241378f804f0060c936428022ff4ffbd51c4c5e5
Juan San Cristobal <130845154+Juan-MSCC@users.noreply.github.com>
2026-03-24 17:39:56 +0000

    Working on Tickets endpoints

--- src/api/models.py
+++ src/api/models.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timezone
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, DateTime
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, DateTime, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 db = SQLAlchemy()
 
@@ -52,17 +53,40 @@ class Client(db.Model):
     full_name: Mapped[str] = mapped_column(String(120), nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
+    tickets: Mapped[List["Ticket"]] = relationship(back_populates="client")
     created_at: Mapped[datetime] = mapped_column(
         DateTime,
         default=datetime.now(timezone.utc)
     )
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
-
     def serialize(self):
         return {
             "id": self.id,
             "full_name": self.full_name,
             "email": self.email,
+            "created_at": self.created_at.isoformat()
+        }
+
+class Ticket(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    client_id: Mapped[int] = mapped_column(ForeignKey("client.id"))
+    id_sucursal: Mapped[int] = mapped_column(Integer, nullable=False)
+    estado: Mapped[str] = mapped_column(String(120), nullable=False)
+    posicion: Mapped[int] = mapped_column(Integer, nullable=False)
+    
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.now(timezone.utc)
+    )
+    client: Mapped["Client"] = relationship(back_populates="tickets")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "id_cliente": self.client_id,
+            "id_sucursal": self.id_sucursal,
+            "estado": self.estado,
+            "posicion": self.posicion,
             "created_at": self.created_at.isoformat()
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,10 +2,10 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Client, Administrador, Tipo
+from api.models import db, User, Client, Administrador, Tipo, Ticket
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from sqlalchemy import select
+from sqlalchemy import select, and_, func
 from .models import Administrador
 
 api = Blueprint('api', __name__)
@@ -261,4 +261,117 @@ def delete_tipo(tipo_id):
     return jsonify({
     "msg": "Tipo eliminado",
     "id": tipo_id
+    }), 200
+
+#######################################################################################
+
+@api.route('/tickets', methods=['GET'])
+def get_tickets():
+
+    all_tickets = list(db.session.execute(select(Ticket)).scalars().all())
+    all_tickets = list(map(lambda ticket: ticket.serialize(), all_tickets))
+    print(all_tickets)
+
+    return jsonify(all_tickets), 200
+
+
+@api.route('/tickets/<int:ticket_id>', methods=['GET'])
+def get_ticket(ticket_id):
+
+    ticket = db.session.execute(select(Ticket).where(Ticket.id == ticket_id)).scalar_one_or_none()
+    if not ticket:
+        return jsonify({"msg": "ticket no encontrado"}), 404
+
+    return jsonify(ticket.serialize()), 200
+
+
+@api.route('/tickets', methods=['POST'])
+def create_ticket():
+ 
+    body = request.get_json()
+    if body is None:
+        return jsonify({"msg": "Request body can't be empty"}), 400
+    if not body.get("client_id"):
+        return jsonify({"msg": "Es necesario especificar client_id"}), 400
+    if not body.get("id_sucursal"):
+        return jsonify({"msg": "Es necesario especificar id_sucursal"}), 400
+
+    ticket_existente = db.session.execute(
+        select(Ticket).where(
+            and_(
+                Ticket.client_id == body['client_id'],
+                Ticket.id_sucursal == body['id_sucursal'],
+                Ticket.estado == 'esperando'
+            )
+        )
+    ).scalar_one_or_none()
+    
+    if ticket_existente:
+        return jsonify({"msg": "Ya existe un ticket activo de este cliente para esta sucursal"}), 400
+    
+    posicion_actual = db.session.execute(
+        select(func.max(Ticket.posicion))
+        .where(
+            and_(
+                Ticket.id_sucursal == body['id_sucursal'],
+                Ticket.estado == 'esperando'
+            )
+        )
+    ).scalar()
+
+    nueva_posicion = 1 if posicion_actual is None else posicion_actual + 1
+
+    new_ticket = Ticket(
+        client_id=body["client_id"],
+        id_sucursal=body["id_sucursal"],
+        estado='esperando',
+        posicion=nueva_posicion
+    )
+    db.session.add(new_ticket)
+    db.session.commit()
+    return jsonify({
+        'msg': 'Ticket creado con exito',
+        'cliente': new_ticket.serialize() 
+    }), 201
+
+@api.route('/tickets/<int:ticket_id>', methods=['PUT'])
+def update_ticket(ticket_id):
+
+    ticket = db.session.get(Ticket, ticket_id)
+    if not ticket:
+        return jsonify({"msg": "Ticket no encontrado"}), 404
+
+    body = request.get_json()
+
+    if not body or 'estado' not in body:
+        return jsonify({'msg': 'Debe especificar el nuevo estado'}), 400
+
+    nuevo_estado = body['estado']
+
+    estados_validos = ["esperando", "en_atencion", "atendido", "cancelado"]
+
+    if nuevo_estado not in estados_validos:
+        return jsonify({"msg": "Estado inválido"}), 400
+    
+
+    db.session.commit()
+
+    return jsonify({
+        'msg': 'Cliente modificado con exito',
+        'body': client.serialize()
+    }), 200
+
+@api.route('/clients/<int:client_id>', methods=['DELETE'])
+def delete_ticket(client_id):
+    client = db.session.get(Client, client_id)
+
+    if not client:
+        return jsonify({"msg": "Cliente no encontrado"}), 404
+
+    db.session.delete(client)
+    db.session.commit()
+
+    return jsonify({
+    "msg": "Cliente eliminado",
+    "id": client_id
     }), 200
