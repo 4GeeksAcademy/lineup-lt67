54bd219b37445f98b8a7c9bd98f3b74dacc88450
ABarboza7 <164107449+ABarboza7@users.noreply.github.com>
2026-04-01 19:34:32 +0000

    add flujo liner

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -54,6 +54,9 @@ import { AddLiner } from "./pages/AddLiner"
 import { EditLiner } from "./pages/EditLiner"
 import { LinerDetail } from "./pages/LinerDetail"
 import { LinerLogin } from "./pages/LinerLogin"
+import { LinerRegister } from "./pages/LinerRegister"
+import { LinerHome } from "./pages/LinerHome"
+import { ProtectedRouteLiner } from "./components/ProtectedRouteLiner"
 
 import { Propuestas } from "./pages/PropuestasCRUD/Propuestas";
 import { PropuestaForm } from "./pages/PropuestasCRUD/PropuestaForm";
@@ -86,6 +89,7 @@ export const router = createBrowserRouter(
         <Route path="/favoritos" element={<Favoritos />} />
         <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/liner/login" element={<LinerLogin />} />
+        <Route path="/liner/register" element={<LinerRegister />} />
 
         {/* ESTABLECIMIENTO NOT PROTECTED*/}
         <Route path="/establecimiento/login" element={<EstablecimientoLogin/>} />
@@ -140,7 +144,9 @@ export const router = createBrowserRouter(
             <Route path="/client/tickets" element={<ClientTickets />} />
         </Route>
 
-
+        <Route element={<ProtectedRouteLiner />}>
+            <Route path="/liner/home" element={<LinerHome />} />
+        </Route>
 
         
       </Route>
